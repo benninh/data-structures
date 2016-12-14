075373081f8534af0420bc1b1b3b6f115563c41a
benninh <bensonninh@gmail.com>
2016-12-14 14:48:04 -0800

    complete remaining tests for functional pattern: stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,19 +1,25 @@
 var Stack = function() {
   var someInstance = {};
 
+  // FILO - first in last out
   // Use an object with numeric keys to store values
   var storage = {};
   var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage['"' + size + '"'] = value;
     size++;
   };
 
   someInstance.pop = function() {
+    
     if (size > 0) {
       size--;
+      var result = storage['"' + size + '"'];
+      delete storage['"' + size + '"'];
     }
+    return result;
   };
 
   someInstance.size = function() {
