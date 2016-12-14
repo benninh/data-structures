6a971310616acdfa3986cd1e89884124e6aa266d
benninh <bensonninh@gmail.com>
2016-12-14 15:54:00 -0800

    complete all remaining tests for functional pattern queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -8,21 +8,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-    size++;
     storage['"' + size + '"'] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
-    var result;
+    var result = 0;
     if (size > 0) {
-      result = storage['0'];
+      result = storage['"' + 0 + '"']; // 'a'
       //do things
       for (let key in storage) {
-        var index = parseInt(key) + 1;
-        index = index.toString();
-        storage[key] = storage[index];
+        var index = parseInt(JSON.parse(key)) + 1;
+        if (index < size) {
+          index = index.toString();
+          storage[key] = storage['"' + index + '"'];          
+        }
       }
       size--;
+      delete storage['"' + size + '"'];
     }
     return result;
   };
@@ -33,3 +36,36 @@ var Queue = function() {
 
   return someInstance;
 };
+
+// Benson's Implementation
+// var Queue = function() {
+//   var someInstance = {};
+
+//   // first in first out
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var size = 0;
+//   var tempSize = 0;
+//   var result = 0;
+//   // Implement the methods below
+
+//   someInstance.enqueue = function(value) {
+//     storage['"' + size + '"'] = value;
+//     size++;
+//   };
+
+//   someInstance.dequeue = function() {
+//     if (size - tempSize > 0) {
+//       result = storage['"' + tempSize + '"'];
+//       delete storage['"' + tempSize + '"'];
+//       tempSize++;
+//     }
+//     return result;
+//   };
+
+//   someInstance.size = function() {
+//     return size - tempSize;
+//   };
+
+//   return someInstance;
+// };
