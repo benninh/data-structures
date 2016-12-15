817278ca04740b107467ef04f70c72c5a3ec1623
benninh <bensonninh@gmail.com>
2016-12-14 19:06:30 -0800

    Stack - setup the instantiation

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.someInstance = {};
+  
+  this.storage = {};
+  this.sizeOfStorage = 0;
+
+  return this.someInstance;
 };
 
+Stack.prototype.push = function(value) {
 
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+
+};
