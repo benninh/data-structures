406a46cfc78373961bda79aac418a25e8bd67988
benninh <bensonninh@gmail.com>
2016-12-14 16:33:48 -0800

    functional shared - queue - finish tests 1,3,5

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,35 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.sizeOfStorage = 0;
+
+  extend(someInstance, queueMethods);
+
+  return someInstance;
+};
+
+
+
+var extend = function(to, toAdd) {
+  for (let key in toAdd) {
+    to[key] = toAdd[key];
+  }
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function() {
+
+};
+
+queueMethods.dequeue = function() {
+
+};
+
+queueMethods.size = function() {
+  return this.sizeOfStorage;
+};
