150f4ff00ae6f293a1de006972f132747837fcd5
benninh <bensonninh@gmail.com>
2016-12-14 19:03:27 -0800

    pseudocode stack pop method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -3,21 +3,28 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = Object.create(stackMethods);
 
+  someInstace.storage = {};
+  someInstace.sizeOfStorage = 0;
+
   return someInstance;
 };
 
 var stackMethods = {};
 
 stackMethods.push = function(value) {
-
+  this.storage[this.sizeOfStorage] = value;
+  this.sizeOfStorage++;
 };
 
 stackMethods.pop = function() {
-
+  // if size is larger than 0 
+    // store the value of key to be deleted
+    // delete key
+    // return deleted value
 };
 
 stackMethods.size = function() {
-
+  return this.sizeOfStorage;
 };
 
 
@@ -26,5 +33,4 @@ var some = Stack();
 some.push(value);
 some.pop();
 some.size();
-
 */
