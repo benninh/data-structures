fb092b927deb8f473c25d367b83d96bf8bd502bd
benninh <bensonninh@gmail.com>
2016-12-14 19:02:14 -0800

    Stack: set up, implement size and push to pass corresponding tests Queue: set up

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
 
+};
+
+stackMethods.pop = function() {
+
+};
+
+stackMethods.size = function() {
+
+};
+
+
+/*
+var some = Stack();
+some.push(value);
+some.pop();
+some.size();
+
+*/
