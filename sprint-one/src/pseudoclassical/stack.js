c1165bbbbc03b9c3e188ebf780fd6c57a80f5c0b
benninh <bensonninh@gmail.com>
2016-12-14 22:39:44 -0800

    complete remaining tests for pseudoclassical Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,13 +1,8 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  someInstance = {};
-  
   this.storage = {};
   this.sizeOfStorage = 0;
-  this.result = 0;
-
-  return this.someInstance;
 };
 
 Stack.prototype.push = function(value) {
@@ -18,10 +13,10 @@ Stack.prototype.push = function(value) {
 Stack.prototype.pop = function() {
   if (this.sizeOfStorage > 0) {
     this.sizeOfStorage--;
-    this.result = this.storage[this.sizeOfStorage];
+    var result = this.storage[this.sizeOfStorage];
     delete this.storage[this.sizeOfStorage];
   }
-  return this.result;
+  return result;
 };
 
 Stack.prototype.size = function() {
