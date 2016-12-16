c7d8eb38a6aa74baaf89163e43310bfd03b199bd
benninh <bensonninh@gmail.com>
2016-12-16 11:39:04 -0800

    complete hash table tests 1-5

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if bucket at index is undefined?
+    // create an empty array?
+  // push v into bucket array 
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // access index bucket array 
+  return this._storage.get(index);
+    // get value from Limited Array
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // check if bucket at index has more than one value
+  this._storage.set(index, undefined);
 };
 
 
