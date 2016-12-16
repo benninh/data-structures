5175d6088be08ac3a3b7b3870d2b90895851c878
benninh <bensonninh@gmail.com>
2016-12-16 11:06:32 -0800

    complete set - basic requirements

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  
+  set.storage = {}; // fix me
+  //
+
+  _.extend(set, setPrototype);
+
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // add item to storage
+  this.storage['"' + item + '"'] = item;
 };
 
 setPrototype.contains = function(item) {
+  return !!this.storage['"' + item + '"'];
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage['"' + item + '"'];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  constant for all 3
  */
